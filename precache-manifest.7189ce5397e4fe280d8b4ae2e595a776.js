51da22f5f165f52acac67b5b7d3299fa8f3c29cd
Nejar Shrestha <nejshrestha@ad.growbydata.com>
2020-06-14 22:43:11 +0545

    Updates

---
+++ precache-manifest.7189ce5397e4fe280d8b4ae2e595a776.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef1dc754d68463b34c09e15fb0010c23",
+    "url": "/new-portal/index.html"
+  },
+  {
+    "revision": "a9bf03111a31281b4878",
+    "url": "/new-portal/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6b811af3689eb06c5aea",
+    "url": "/new-portal/static/js/2.0663cec6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/new-portal/static/js/2.0663cec6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9bf03111a31281b4878",
+    "url": "/new-portal/static/js/main.bfd14e1b.chunk.js"
+  },
+  {
+    "revision": "8ea287f0e5dd4a40f89c",
+    "url": "/new-portal/static/js/runtime-main.4a0a4c16.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/new-portal/static/media/logo.5d5d9eef.svg"
+  }
+]);
