08515bb26c5222343cd9a686e1393a0317c71fdc
Nejar Shrestha <nejshrestha@ad.growbydata.com>
2020-06-24 00:35:32 +0545

    added link to title of news

--- src/components/dashboard/NewsCard.js
+++ src/components/dashboard/NewsCard.js
@@ -1,12 +1,17 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
 const NewsCard = (props) => {
   return (
     <div className="card">
       <img src={props.urlToImage} className="card-img-top" alt={props.title} />
       <div className="card-body">
-        <div className="card-title">{props.title}</div>
+        <div className="card-title">
+          <a href={props.url} target="_blank">
+            {props.title}
+          </a>
+        </div>
         <div className="card-text">{props.description}</div>
       </div>
 
